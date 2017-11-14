bd68f3d91a56ecc035535857064af0458649cc30
Harsh Goel <harsh_goel@ymail.com>
2017-11-13 16:57:27 -0800

    TwilioServerSetupDONE

--- djtwilio/sms/views.py
+++ djtwilio/sms/views.py
@@ -1,3 +1,4 @@
+import json
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
@@ -8,14 +9,14 @@ from twilio.twiml.messaging_response import MessagingResponse
 def sms_response(request):
     str11 = request.POST.get('Body','')
     print(str11)
-
+    print(request.__dict__)
     # Start our TwiML response
     resp = MessagingResponse()
-
     # Add a text message
     msg = resp.message("Check out this sweet owl!")
 
     # Add a picture message
     msg.media("https://demo.twilio.com/owl.png")
+
 
     return HttpResponse(str(resp))
